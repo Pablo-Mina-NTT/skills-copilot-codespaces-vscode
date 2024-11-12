5901037f6357ed3387b45bc4de9f30ee0bd97e59
Pablo Mina <123571726+Pablo-Mina-NTT@users.noreply.github.com>
2024-11-12 16:53:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 25,
+        skills: ["Javascript", "HTML", "CSS"],
+        address: {
+            city: "New York",
+            state: "NY"
+        }
+    };
+    return member;
+}
